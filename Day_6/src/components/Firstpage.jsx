1d113f1c6afb19ea3a9835acbcc7caec0531685c
pradeep-2003 <127285234+pradeep-2003@users.noreply.github.com>
2024-01-31 14:17:48 +0530

    commit

---
+++ Day_6/src/components/Firstpage.jsx
@@ -0,0 +1,16 @@
+import '..//assets/css/firstpage.css'
+
+export default function Firstpage() {
+  return (
+    <>
+    <div className="first_page">
+    <div className="home_head">
+        <a href='/login'>LOGIN</a>
+        <a href='/SIGNUP'>SIGNUP</a>
+    </div>
+      <h1>Welcome to Corporate Pulse Event</h1>
+      <p>We Help You Create A Memorable Event With Lasting Impression.</p>
+    </div>
+    </>
+  )
+}
